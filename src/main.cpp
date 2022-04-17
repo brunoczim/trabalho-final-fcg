3441ca0b693c300cfbb02930cacf6f587580f584
brunoczim <brunoczim@gmail.com>
2022-04-17 18:53:33 -0300

    main structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,25 @@
 #include "block.hpp"
 #include "gpu.hpp"
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
 void ErrorCallback(int error, const char* description);
 void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
 void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
 void CursorPosCallback(GLFWwindow* window, double xpos, double ypos);
 void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+
+void LoadShader(const char* filename, GLuint shader_id);
+GLuint LoadShader_Vertex(const char* filename);   // Carrega um vertex shader
+GLuint LoadShader_Fragment(const char* filename); // Carrega um fragment shader
+GLFWwindow *createWindow(void);
+void setupInputCallbacks(GLFWwindow *window);
+void printGlInfo();
+void setupFrameBufferSize(GLFWwindow *window);
+
+float g_ScreenRatio = 1.0f;
 
 int main(int argc, char const *argv[])
 {
@@ -20,21 +33,7 @@ int main(int argc, char const *argv[])
     // Definimos o callback para impressão de erros da GLFW no terminal
     glfwSetErrorCallback(ErrorCallback);
 
-    // Pedimos para utilizar OpenGL versão 3.3 (ou superior)
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-
-    #ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    #endif
-
-    // Pedimos para utilizar o perfil "core", isto é, utilizaremos somente as
-    // funções modernas de OpenGL.
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    // Criamos uma janela do sistema operacional, com 800 colunas e 600 linhas
-    // de pixels, e com título "INF01047 ...".
-    GLFWwindow* window;
-    window = glfwCreateWindow(800, 600, "INF01047 - 313985 e 314256 - Bruno C Zimmermann e João G Zandoná", NULL, NULL);
+    GLFWwindow *window = createWindow();
     if (!window)
     {
         glfwTerminate();
@@ -42,21 +41,88 @@ int main(int argc, char const *argv[])
         std::exit(EXIT_FAILURE);
     }
 
-    // Definimos a função de callback que será chamada sempre que o usuário
-    // pressionar alguma tecla do teclado ...
-    glfwSetKeyCallback(window, KeyCallback);
-    // ... ou clicar os botões do mouse ...
-    glfwSetMouseButtonCallback(window, MouseButtonCallback);
-    // ... ou movimentar o cursor do mouse em cima da janela ...
-    glfwSetCursorPosCallback(window, CursorPosCallback);
-    // ... ou rolar a "rodinha" do mouse.
-    glfwSetScrollCallback(window, ScrollCallback);
+    setupInputCallbacks(window);
+
+    // Indicamos que as chamadas OpenGL deverão renderizar nesta janela
+    glfwMakeContextCurrent(window);
 
     // Carregamento de todas funções definidas por OpenGL 3.3, utilizando a
     // biblioteca GLAD.
     gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
 
-    WorldBlockMatrix blockMatrix;
+    setupFramebufferSize(window);
+
+    printGlInfo();
+
+    // Carregamos os shaders de vértices e de fragmentos que serão utilizados
+    // para renderização. Veja slides 176-196 do documento Aula_03_Rendering_Pipeline_Grafico.pdf.
+    //
+    // Note que o caminho para os arquivos "shader_vertex.glsl" e
+    // "shader_fragment.glsl" estão fixados, sendo que assumimos a existência
+    // da seguinte estrutura no sistema de arquivos:
+    //
+    //    /
+    //    |
+    //    +--+ bin/
+    //    |  |
+    //    |  +--+ Release/  (ou Debug/ ou Linux/)
+    //    |     |
+    //    |     o-- main.exe
+    //    |
+    //    +--+ src/
+    //       |
+    //       o-- shader_vertex.glsl
+    //       |
+    //       o-- shader_fragment.glsl
+    //       |
+    //       o-- ...
+    //
+    GLuint vertex_shader_id = LoadShader_Vertex("../../src/shader_vertex.glsl");
+    GLuint fragment_shader_id = LoadShader_Fragment("../../src/shader_fragment.glsl");
+
+    // Criamos um programa de GPU utilizando os shaders carregados acima
+    GLuint program_id = CreateGpuProgram(vertex_shader_id, fragment_shader_id);
+
+    // Habilitamos o Z-buffer. Veja slides 104-116 do documento Aula_09_Projecoes.pdf.
+    glEnable(GL_DEPTH_TEST);
+
+    while (!glfwWindowShouldClose(window)) {
+        // Aqui executamos as operações de renderização
+
+        // Definimos a cor do "fundo" do framebuffer como branco.  Tal cor é
+        // definida como coeficientes RGBA: Red, Green, Blue, Alpha; isto é:
+        // Vermelho, Verde, Azul, Alpha (valor de transparência).
+        // Conversaremos sobre sistemas de cores nas aulas de Modelos de Iluminação.
+        //
+        //           R     G     B     A
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+        // "Pintamos" todos os pixels do framebuffer com a cor definida acima,
+        // e também resetamos todos os pixels do Z-buffer (depth buffer).
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Pedimos para a GPU utilizar o programa de GPU criado acima (contendo
+        // os shaders de vértice e fragmentos).
+        glUseProgram(program_id);
+
+        // O framebuffer onde OpenGL executa as operações de renderização não
+        // é o mesmo que está sendo mostrado para o usuário, caso contrário
+        // seria possível ver artefatos conhecidos como "screen tearing". A
+        // chamada abaixo faz a troca dos buffers, mostrando para o usuário
+        // tudo que foi renderizado pelas funções acima.
+        // Veja o link: https://en.wikipedia.org/w/index.php?title=Multiple_buffering&oldid=793452829#Double_buffering_in_computer_graphics
+        glfwSwapBuffers(window);
+
+        // Verificamos com o sistema operacional se houve alguma interação do
+        // usuário (teclado, mouse, ...). Caso positivo, as funções de callback
+        // definidas anteriormente usando glfwSet*Callback() serão chamadas
+        // pela biblioteca GLFW.
+        glfwPollEvents();
+
+    }
+
+    // Finalizamos o uso dos recursos do sistema operacional
+    glfwTerminate();
     return 0;
 }
 
@@ -88,3 +154,182 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mod)
 {
 }
 
+// Definição da função que será chamada sempre que a janela do sistema
+// operacional for redimensionada, por consequência alterando o tamanho do
+// "framebuffer" (região de memória onde são armazenados os pixels da imagem).
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+    // Indicamos que queremos renderizar em toda região do framebuffer. A
+    // função "glViewport" define o mapeamento das "normalized device
+    // coordinates" (NDC) para "pixel coordinates".  Essa é a operação de
+    // "Screen Mapping" ou "Viewport Mapping" vista em aula ({+ViewportMapping2+}).
+    glViewport(0, 0, width, height);
+
+    // Atualizamos também a razão que define a proporção da janela (largura /
+    // altura), a qual será utilizada na definição das matrizes de projeção,
+    // tal que não ocorra distorções durante o processo de "Screen Mapping"
+    // acima, quando NDC é mapeado para coordenadas de pixels. Veja slides 205-215 do documento Aula_09_Projecoes.pdf.
+    //
+    // O cast para float é necessário pois números inteiros são arredondados ao
+    // serem divididos!
+    g_ScreenRatio = (float) width / height;
+}
+
+// Carrega um Vertex Shader de um arquivo. Veja definição de LoadShader() abaixo.
+GLuint LoadShader_Vertex(const char* filename)
+{
+    // Criamos um identificador (ID) para este shader, informando que o mesmo
+    // será aplicado nos vértices.
+    GLuint vertex_shader_id = glCreateShader(GL_VERTEX_SHADER);
+
+    // Carregamos e compilamos o shader
+    LoadShader(filename, vertex_shader_id);
+
+    // Retorna o ID gerado acima
+    return vertex_shader_id;
+}
+
+// Carrega um Fragment Shader de um arquivo. Veja definição de LoadShader() abaixo.
+GLuint LoadShader_Fragment(const char* filename)
+{
+    // Criamos um identificador (ID) para este shader, informando que o mesmo
+    // será aplicado nos fragmentos.
+    GLuint fragment_shader_id = glCreateShader(GL_FRAGMENT_SHADER);
+
+    // Carregamos e compilamos o shader
+    LoadShader(filename, fragment_shader_id);
+
+    // Retorna o ID gerado acima
+    return fragment_shader_id;
+}
+
+// Função auxilar, utilizada pelas duas funções acima. Carrega código de GPU de
+// um arquivo GLSL e faz sua compilação.
+void LoadShader(const char* filename, GLuint shader_id)
+{
+    // Lemos o arquivo de texto indicado pela variável "filename"
+    // e colocamos seu conteúdo em memória, apontado pela variável
+    // "shader_string".
+    std::ifstream file;
+    try {
+        file.exceptions(std::ifstream::failbit);
+        file.open(filename);
+    } catch ( std::exception& e ) {
+        fprintf(stderr, "ERROR: Cannot open file \"%s\".\n", filename);
+        std::exit(EXIT_FAILURE);
+    }
+    std::stringstream shader;
+    shader << file.rdbuf();
+    std::string str = shader.str();
+    const GLchar* shader_string = str.c_str();
+    const GLint   shader_string_length = static_cast<GLint>( str.length() );
+
+    // Define o código do shader GLSL, contido na string "shader_string"
+    glShaderSource(shader_id, 1, &shader_string, &shader_string_length);
+
+    // Compila o código do shader GLSL (em tempo de execução)
+    glCompileShader(shader_id);
+
+    // Verificamos se ocorreu algum erro ou "warning" durante a compilação
+    GLint compiled_ok;
+    glGetShaderiv(shader_id, GL_COMPILE_STATUS, &compiled_ok);
+
+    GLint log_length = 0;
+    glGetShaderiv(shader_id, GL_INFO_LOG_LENGTH, &log_length);
+
+    // Alocamos memória para guardar o log de compilação.
+    // A chamada "new" em C++ é equivalente ao "malloc()" do C.
+    GLchar* log = new GLchar[log_length];
+    glGetShaderInfoLog(shader_id, log_length, &log_length, log);
+
+    // Imprime no terminal qualquer erro ou "warning" de compilação
+    if ( log_length != 0 )
+    {
+        std::string  output;
+
+        if ( !compiled_ok )
+        {
+            output += "ERROR: OpenGL compilation of \"";
+            output += filename;
+            output += "\" failed.\n";
+            output += "== Start of compilation log\n";
+            output += log;
+            output += "== End of compilation log\n";
+        }
+        else
+        {
+            output += "WARNING: OpenGL compilation of \"";
+            output += filename;
+            output += "\".\n";
+            output += "== Start of compilation log\n";
+            output += log;
+            output += "== End of compilation log\n";
+        }
+
+        std::cerr << output;
+    }
+
+    // A chamada "delete" em C++ é equivalente ao "free()" do C
+    delete [] log;
+}
+
+GLFWwindow *createWindow(void)
+{
+        // Pedimos para utilizar OpenGL versão 3.3 (ou superior)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    #ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
+
+    // Pedimos para utilizar o perfil "core", isto é, utilizaremos somente as
+    // funções modernas de OpenGL.
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // Criamos uma janela do sistema operacional, com 800 colunas e 600 linhas
+    // de pixels, e com título "INF01047 ...".
+    GLFWwindow* window;
+    window = glfwCreateWindow(800, 600, "INF01047 - 313985 e 314256 - Bruno C Zimmermann e João G Zandoná", NULL, NULL);
+    return window;
+}
+
+void setupInputCallbacks(GLFWwindow *window)
+{
+    // Definimos a função de callback que será chamada sempre que o usuário
+    // pressionar alguma tecla do teclado ...
+    glfwSetKeyCallback(window, KeyCallback);
+    // ... ou clicar os botões do mouse ...
+    glfwSetMouseButtonCallback(window, MouseButtonCallback);
+    // ... ou movimentar o cursor do mouse em cima da janela ...
+    glfwSetCursorPosCallback(window, CursorPosCallback);
+    // ... ou rolar a "rodinha" do mouse.
+    glfwSetScrollCallback(window, ScrollCallback);
+}
+
+void printGlInfo()
+{
+    // Imprimimos no terminal informações sobre a GPU do sistema
+    const GLubyte *vendor      = glGetString(GL_VENDOR);
+    const GLubyte *renderer    = glGetString(GL_RENDERER);
+    const GLubyte *glversion   = glGetString(GL_VERSION);
+    const GLubyte *glslversion = glGetString(GL_SHADING_LANGUAGE_VERSION);
+
+    std::cout   << "GPU: "
+                << vendor
+                << ", "
+                << renderer
+                << ", OpenGL: "
+                << glversion
+                << ", GLSL: "
+                << glslversion
+                << std::endl;
+}
+
+void setupFrameBufferSize(GLFWwindow *window)
+{
+    // Definimos a função de callback que será chamada sempre que a janela for
+    // redimensionada, por consequência alterando o tamanho do "framebuffer"
+    // (região de memória onde são armazenados os pixels da imagem).
+    glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
+    FramebufferSizeCallback(window, 800, 600); // Forçamos a chamada do callback acima, para definir g_ScreenRatio.
+}
