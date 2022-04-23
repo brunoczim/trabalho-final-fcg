a7555f0578f7be44640add04e86ded16539730fe
brunoczim <brunoczim@gmail.com>
2022-04-23 15:48:15 -0300

    include a way for indexing via size_t's

--- include/blocks.hpp
+++ include/blocks.hpp
@@ -13,14 +13,34 @@ enum Block {
     BLOCK_GRASS
 };
 
+struct WorldPoint {
+public:
+    size_t x;
+    size_t y;
+    size_t z;
+
+    WorldPoint(size_t x, size_t y, size_t z);
+    WorldPoint(glm::vec3 point);
+
+    glm::vec3 ToGlm();
+};
+
 struct WorldBlockMatrix {
+private:
     Block blocks[WORLD_SIZE_X][WORLD_SIZE_Y][WORLD_SIZE_Z];
 
+public:
     WorldBlockMatrix();
+
+    static const WorldPoint SIZE;
 
     Block &operator [] (glm::vec3 point);
 
+    Block &operator [] (WorldPoint point);
+
     Block const &operator [] (glm::vec3 point) const;
+
+    Block const &operator [] (WorldPoint point) const;
 };
 
 #endif // BLOCK_HPP
--- src/blocks.cpp
+++ src/blocks.cpp
@@ -1,15 +1,42 @@
 #include "blocks.hpp"
+#include <math.h>
+
+const WorldPoint WorldBlockMatrix::SIZE = WorldPoint(WORLD_SIZE_X, WORLD_SIZE_Y, WORLD_SIZE_Z);
+
+WorldPoint::WorldPoint(size_t x, size_t y, size_t z): x(x), y(y), z(z)
+{
+}
+
+WorldPoint::WorldPoint(glm::vec3 point):
+    WorldPoint((size_t) round(point.x), (size_t) round(point.y), (size_t) round(point.z))
+{
+}
+
+glm::vec3 WorldPoint::ToGlm()
+{
+    return glm::vec3(this->x, this->y, this->z);
+}
 
 WorldBlockMatrix::WorldBlockMatrix(): blocks { BLOCK_AIR }
 {
 }
 
+Block &WorldBlockMatrix::operator [] (WorldPoint point)
+{
+    return this->blocks[point.x][point.y][point.z];
+}
+
 Block &WorldBlockMatrix::operator [] (glm::vec3 point)
 {
-    return blocks[(size_t) point.x][(size_t) point.y][(size_t) point.z];
+    return (*this)[WorldPoint(point)];
+}
+
+Block const &WorldBlockMatrix::operator [] (WorldPoint point) const
+{
+    return this->blocks[point.x][point.y][point.z];
 }
 
 Block const &WorldBlockMatrix::operator [] (glm::vec3 point) const
 {
-    return blocks[(size_t) point.x][(size_t) point.y][(size_t) point.z];
+    return (*this)[WorldPoint(point)];
 }
